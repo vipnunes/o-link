15b472c11f7ca0e173bf76321861a74ff6b1e4db
aviiciii <laavesh1@gmail.com>
2023-06-23 15:13:58 +0530

    trying to change the url on hover

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ btn.addEventListener('click', () => {
         if (url.endsWith('/')) {
             url = url.slice(0, -1)
         }
-        
+
 
 
         console.log(url);
@@ -35,3 +35,33 @@ btn.addEventListener('click', () => {
     })
     }
 )
+
+
+// show link on hover
+
+const links = document.querySelectorAll('.link-a')
+links.forEach(link => {
+
+    let url = link.href;
+    console.log(url);
+    
+    if (url.includes('https://')) {
+        url = url.replace('https://', '')
+    } else {
+        url = url.replace('http://', '')
+    }
+
+    // remove trailing slash
+    if (url.endsWith('/')) {
+        url = url.slice(0, -1)
+    }
+
+    url = ` - ${url}`
+
+    link.addEventListener('mouseenter', () => {
+        link.innerHTML += url
+    })
+    link.addEventListener('mouseleave', () => {
+        link.innerHTML = link.dataset.name
+    })
+})
