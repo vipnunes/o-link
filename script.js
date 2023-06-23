d6505b92f23648d589d8915cc8bad84846f6dcc5
aviiciii <laavesh1@gmail.com>
2023-06-23 19:48:43 +0530

    added toggle for the feature

--- script.js
+++ script.js
@@ -1,71 +1,63 @@
+addEventListener('DOMContentLoaded', () => {
+
+    // copyright
+    document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
 
 
-// copyright
-document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+    // invert links
 
+    const toggle = document.getElementById('invert');
 
-// invert links
-
-const btn = document.getElementById('invert')
-
-btn.addEventListener('click', () => {
-
-    console.log('clicked')
-
-    const links = document.querySelectorAll('.link-a')
-    links.forEach(link => {
-        let url = link.href;
-        console.log(url);
+    // check if invert is set
+    if (localStorage.getItem('invert') === 'true') {
         
-        if (url.includes('https://')) {
-            url = url.replace('https://', '')
-        } else {
-            url = url.replace('http://', '')
+        toggle.checked = true;
+        invert();
+    }
+
+    toggle.addEventListener('change', () => {
+        console.log(toggle.checked);
+        localStorage.setItem('invert', toggle.checked)
+        invert();
+    })
+
+
+
+    function invert() { 
+    // show link on hover
+        const toggle = document.getElementById('invert');
+        console.log(toggle.checked);
+        if (toggle.checked) {
+            const links = document.querySelectorAll('.link-a')
+            links.forEach(link => {
+
+                let url = link.href;
+                let name = link.dataset.name;
+                console.log(url);
+                
+                if (url.includes('https://')) {
+                    url = url.replace('https://', '');
+                } else {
+                    url = url.replace('http://', '');
+                }
+
+                // remove trailing slash
+                if (url.endsWith('/')) {
+                    url = url.slice(0, -1);
+                }
+
+                url = `${name} - ${url}`
+
+                link.addEventListener('mouseenter', () => {
+                    link.style.opacity = 0;
+                    link.innerHTML = url;
+                    link.style.opacity = 1;
+                })
+                link.addEventListener('mouseleave', () => {
+                    link.innerHTML = link.dataset.name
+                })
+            })
+
         }
-
-        // remove trailing slash
-        if (url.endsWith('/')) {
-            url = url.slice(0, -1)
-        }
-
-
-
-        console.log(url);
-        link.innerHTML = url;
-    })
     }
-)
-
-
-// show link on hover
-
-const links = document.querySelectorAll('.link-a')
-links.forEach(link => {
-
-    let url = link.href;
-    let name = link.dataset.name;
-    console.log(url);
-    
-    if (url.includes('https://')) {
-        url = url.replace('https://', '');
-    } else {
-        url = url.replace('http://', '');
-    }
-
-    // remove trailing slash
-    if (url.endsWith('/')) {
-        url = url.slice(0, -1);
-    }
-
-    url = `${name} - ${url}`
-
-    link.addEventListener('mouseenter', () => {
-        link.style.opacity = 0;
-        link.innerHTML = url;
-        link.style.opacity = 1;
-    })
-    link.addEventListener('mouseleave', () => {
-        link.innerHTML = link.dataset.name
-    })
 })
-
