6b4b3009f6bd113e64b76dcc5d2e205c1080aa4e
aviiciii <laavesh1@gmail.com>
2023-06-23 14:57:55 +0530

    changing from name of links to the actual link

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 
 // copyright
 document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+
+
+// invert links
+
+const btn = document.getElementById('invert')
+
+btn.addEventListener('click', () => {
+
+    console.log('clicked')
+
+    const links = document.querySelectorAll('.link-a')
+    links.forEach(link => {
+        link.innerHTML = link.href
+    })
+    }
+)
